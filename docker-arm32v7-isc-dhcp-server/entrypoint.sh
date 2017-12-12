93b2beb0d2bb3f8ac5e1f9ec9fdff0aed00b23c6
atomicbaum1 <atomicbaum1@gmail.com>
2017-12-11 21:50:52 -0800

    Alpine working with dhcp installed.  DHCP is not working on x86_64 test platform with libgcc_s.so.1 error

--- docker-arm32v7-isc-dhcp-server/entrypoint.sh
+++ docker-arm32v7-isc-dhcp-server/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # Start DHCP as a foreground process
-/usr/sbin/dhcp -f -lf /bar/lib/dhcp/dhcpd.leases -cf /etc/dhcp/dhcp.conf
-
+#/usr/sbin/dhcp -f -lf /bar/lib/dhcp/dhcpd.leases -cf /etc/dhcp/dhcp.conf
+$1
